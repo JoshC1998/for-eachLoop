6044bebcf1e4ba1390d5301202fbaa720835d52d
Joshua Cruz <jcruz1998@gmail.com>
2024-11-03 17:52:21 -0500

    for-each Loop

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //String[] animals = {"cat", "dog", "rat", "bird"};
+        ArrayList<String> animals = new ArrayList<String>();
+
+        animals.add("dog");
+        animals.add("cat");
+        animals.add("rat");
+        animals.add("bird");
+
+        for(String i: animals) {
+            System.out.println(i);
+        }
+
+    }
+}
